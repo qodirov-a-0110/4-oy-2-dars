06228e88f0e6d231c9b29c8bd3d3eacec1cdcc12
qodirov-a-0110 <qodirovabdumajid03@gmail.com>
2024-05-20 20:54:03 +0500

    master finished

---
+++ js/index.js
@@ -0,0 +1,182 @@
+
+// 1-masala
+// let a = +prompt("a");
+// if (a > 0) {
+//     n++;
+// } else {a
+// }
+// console.log(a);
+
+// 2-masala
+// let a = +prompt("a");
+// if (a > 0) {
+//     a++;
+// } else if (a < 0) {
+//     a -=2;
+// }
+// console.log(a);
+
+// 3-masala
+// let a = +prompt("a");
+// if (a > 0) {
+//     a++;
+// } else if (a = 0) {
+//     a = 10;
+// } else if (a < 0) {
+//     a -=2;
+// }
+// console.log(n);
+
+// 4-masala
+// let a = +prompt("a");
+// let b = +prompt("b ni kiriting");
+// let c = +prompt("c ni kiriting");
+// let musbat = 0;
+// if (a > 0) {
+//   musbat = musbat + 1;
+// }
+// if (b > 0) {
+//   musbat = musbat + 1;
+// }
+// if (c > 0) {
+//   musbat = musbat + 1;
+// }
+// console.log(musbat);
+
+// 5- masala
+// let a = +prompt("a");
+// let b = +prompt("b ni kiriting");
+// let c = +prompt("c ni kiriting");
+// let d = +prompt("d ni kiriting");
+
+
+// let musbat = 0;
+// let manfiy = 0;
+
+// if (a > 0) {
+//   musbat = musbat + 1;
+// } else  if (a < 0) {
+//   manfiy = manfiy + 1;
+// }
+
+// if (b % 2 == 1) {
+//   musbat = musbat + 1;
+// } else  if (b < 0){
+//   manfiy = manfiy + 1;
+// }
+
+// if (c % 2 == 1) {
+//   musbat = musbat + 1;
+// } else  if (c < 0){
+//   manfiy = manfiy + 1;
+// }
+
+// if (d % 2 == 1) {
+//   musbat = musbat + 1;
+// } else if (d < 0) {
+//   manfiy = manfiy + 1;
+// }
+// console.log(manfiy);
+// console.log(musbat);
+
+
+
+
+// 6-masala
+// let a = +prompt("a");
+// let b = +prompt("b");
+// let max = 0;
+
+// if (a > b) {
+//     max = a;
+// } else {
+//     max = b
+// }
+
+// 7-masala
+// let a = +prompt("a  ");
+// let b = +prompt("b  ");
+// let max;
+// if (a > b) {
+//     max=1;
+// } else {
+//     max=2
+// }
+
+// 8-masala
+// let a = +prompt("a  ");
+// let b = +prompt("b  ");
+
+// let max = 0;
+// let min = 0;
+
+// if (a > b) {
+//   max = max + a;
+//   min = min + b;
+// } else if (b > a) {
+//   max = max + b;
+//   min = min + a;
+// }
+
+// console.log(max);
+// console.log(min);
+
+// 9-masala
+// let A = +prompt("A  ");
+// let B = +prompt("B  ");
+// let c;
+// if (A>B) {
+//     c = B;
+//     B = A;
+//     A = c;
+// }
+
+// 10-masala
+// let A = +prompt("A");
+// let B = +prompt("B");
+// let max;
+// if (A == B) {
+//   A = 0;
+//   B = 0;
+// } else if (A > B) {
+//   max = A;
+//   A = A;
+//   B = A;
+// } else if (B > A) {
+//   max = B;
+//   B = B;
+//   A = B;
+// }
+
+// 11- masala
+// let A = +prompt("A");
+// let B = +prompt("B");
+
+// if (A == B) {
+//     A = 0;
+//     B = 0;
+// } else if (A > B) {
+//     A = A;
+//     B =A;
+// } else if (B > A) {
+//     A = B;
+//     B = B;
+// }
+
+// 12- masala
+// let a = +prompt(");
+// let b = +prompt("b");
+// let c = +prompt("c");
+// let min;
+// if (a > b) {
+//     min = a;
+// } else {
+//     min = b
+// }
+// if (min > c) {
+//     min = c;
+// }
+// console.log(min);
+
+
+
